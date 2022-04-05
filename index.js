aeeb060749fe42980290a92af6e8055637ab757d
Pavel <pashutka.koltusha@gmail.com>
2022-04-05 17:18:31 +0300

    feat: added index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const stream = require("stream");
+const fs = require("fs")
+const util = require("util")
+const program = require("commander")
+// import program from "commander"
+
+const Validator = require("./helpers/validator.class");
+const FooTransform = require("./helpers/fooTransform.class");
+
+const pipeline = util.promisify(stream.pipeline)
+const { log, info } = console;
+const errorMessage = ("✘ Erorr");
+const successMessage = ("✔ Successful");
+const codeMessage = ("Code");
+
+const actionHandler = async () => { 
+    let {input, output, action} = program.opts();
+
+    Validator.isEmpty(input) && info(('Input text [press enter to run | press ctrl + c to exit]:'));
+    try {
+        await pipeline(
+            !Validator.isEmpty(input) ? fs.createReadStream(input) : process.stdin,
+            new FooTransform(action),
+            !Validator.isEmpty(output) ? fs.createWriteStream(output, {flags: 'a'}) : process.stdout
+        )
+        log(`${successMessage} text ${action}`)
+    } catch (e) {
+        process.stderr.write(`${errorMessage} ${e.message}\n`);
+        process.exit(1);
+    }
+};
+
+process.stdin.setEncoding("utf8");
+process.on("exit", (code) => log(`${codeMessage} ${code}`));
+process.on("SIGINT", () => {
+  process.exit(0);
+});
+
+program
+  .requiredOption("-a --action <action>", "An action encode/decode")
+  .option("-i, --input <filename>", "An input file")
+  .option("-o --output <filename>", "An output file")
+  .action(actionHandler);
+
+program.parse(process.argv);
